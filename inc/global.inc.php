4a403af3c892677daf26c59632896bf5b41e53aa
Vladimir Sobolev <v.sobolev@gmail.com>
2026-01-19 16:04:18 +0200

    Fix PHP 8.1+ deprecation warning in version formatting (proper int conversion)

--- inc/global.inc.php
+++ inc/global.inc.php
@@ -1292,11 +1292,10 @@ function get_current_pml_version()
 		}
 		
 		// Format as x.x.xx (e.g., 1738 -> 1.7.38)
-		$v = sprintf( '%d.%d.%02d' , 
-			(int)floor( $base_version / 1000 ) ,           // first digit
-			(int)floor( ( $base_version / 100 ) % 10 ) ,    // second digit
-			(int)( $base_version % 100 )                    // last two digits
-		);
+		$first_digit  = (int)floor( $base_version / 1000 );
+		$second_digit = (int)floor( ( $base_version / 100 ) % 10 );
+		$last_two     = (int)( $base_version % 100 );
+		$v = sprintf( '%d.%d.%02d' , $first_digit , $second_digit , $last_two );
 	}
 
 	return $v;
