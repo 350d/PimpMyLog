d59add6544895f52df7f543194d390c89c260a70
Vladimir Sobolev <v.sobolev@gmail.com>
2026-01-19 14:38:30 +0200

    Add version.js with current version 1.7.15

---
+++ version.js
@@ -0,0 +1,12 @@
+/*PSK*/pml_version_cb(/*PSK*/
+{
+	"version"     : "1.7.15",
+	"versiondevh" : "",
+	"messages"    : {},
+	"changelog"   : {
+		"1.7.15" : {
+			"welcome" : "Block-based parsing support for multi-line logs (MySQL Slow Log), PHP 8.0+ compatibility, ServerPilot configuration examples"
+		}
+	}
+}
+);
